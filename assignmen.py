080c011b8cb4119f02b5ab99d66ae589c34f4bb2
AlexLihenzero <alexlyhenzero@gmail.com>
2026-03-12 17:16:06 +0300

    Document questions and examples in assignmen.py
    Added comments and examples for various questions.

---
+++ assignmen.py
@@ -0,0 +1,29 @@
+# Question 4
+# printing Hello AI student
+# print("Hello AI students")
+
+# Question 5
+* Name
+* Age
+* City
+# Printing the varriables labeled
+# print("Name:", name)
+# Print("Age:",age)
+# Print("City:", city)
+
+# Question 6
+# printing first language
+# ["python","java","c++","Go","JavaScrip"]
+# print(language[0])
+
+# Question 7
+# student = {
+"name": "John",
+"age": 21,
+"course": "AI"
+}
+# print(student["name"])
+
+## Bonus Question: difference between a list and a Dictionary
+* **A list** is an ordered collection of items.
+* **A Dictionary** is a collection of key value pairs.
